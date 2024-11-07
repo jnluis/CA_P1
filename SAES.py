a9f237d661812b36e6c0dd198fe79070b3e954fa
Ricardo Quintaneiro <97852246+ricardoquintaneiro@users.noreply.github.com>
2024-11-07 16:25:00 +0000

    Clean test code

--- SAES.py
+++ SAES.py
@@ -235,14 +235,6 @@ class AES(object):
         offset = round * 16
         exkey = self.exkey
 
-        # CÓDIGO DE TESTE PARA O C
-        exkey_temp = exkey[offset:offset + 16]
-        if round == self.modified_round_number:
-            for i in range(16):
-                exkey_temp[i] ^= self.modified_round_skey[i % 8]
-            print("Round key after modification: ", exkey_temp.tobytes().hex())
-        # CÓDIGO DE TESTE PARA O C
-
         for i in range(16):
             block[i] ^= exkey[offset + i]
 
